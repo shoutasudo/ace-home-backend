b97e84d8a06cdfeb5d7c00eb38b8d660fd594959
Shin.Murakami <s.murakami.apicalpoint@gmail.com>
2024-06-13 09:33:40 +0900

    feat: contact crud
    - contactテーブル作成

---
+++ database/migrations/2024_06_12_020310_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('contact_type');
+            $table->string('name');
+            $table->string('company_name')->nullable();
+            $table->string('tel_number');
+            $table->string('email');
+            $table->string('content');
+            $table->tinyInteger('status')->comment('0:未読, 1:既読')->default(0);
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
